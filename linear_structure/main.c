c24ed961a45f531cfa7d0d97e8834be772db0ff9
rbm0524 <rbm0524@naver.com>
2025-12-05 16:28:52 +0900

    feat: 2중 연결 리스트로 전환

--- linear_structure/main.c
+++ linear_structure/main.c
@@ -7,38 +7,41 @@ typedef struct USERDATA
 	int age;
 	char name[32];
 	char phone[32];
+	struct USERDATA* pPrev;
 	struct USERDATA* pNext;
 } USERDATA;
 
 // USERDATA* g_pHeadNode = NULL;
-USERDATA g_HeadNode = { 0, "_DummyNode_"};
+USERDATA g_HeadNode = { 0, "_DummyHead_"};
+USERDATA g_TailNode = { 0, "_DummyTail_" };
 
-void AddNewNode(int age, const char *pszName, const char *pszPhone) {
+// queue 방식으로 넣기
+void AddNewNode (int age, const char* pszName, const char* pszPhone) {
 	USERDATA* pNewNode = (USERDATA*)malloc(sizeof(USERDATA));
 	memset(pNewNode, 0, sizeof(USERDATA));
 
 	pNewNode -> age = age;
 	strcpy_s(pNewNode->name, sizeof(pNewNode -> name), pszName);
 	strcpy_s(pNewNode->phone, sizeof(pNewNode->phone), pszPhone);
-	pNewNode->pNext = NULL;
+	
+	USERDATA* pPrevNode = g_TailNode.pPrev;
+	pNewNode -> pPrev = pPrevNode;
+	pNewNode -> pNext = &g_TailNode;
 
-	// head에 넣기 (stack 구조)
-	//pNewNode->pNext = g_pHeadNode;
-	//g_pHeadNode = pNewNode;
-		
-	// tail에 넣기 (queue 구조)
-	USERDATA* pTail = &g_HeadNode;
-	while (pTail -> pNext != NULL) {
-		pTail = pTail->pNext;
-	}
-	pTail->pNext = pNewNode;
+	pPrevNode -> pNext = pNewNode;
+	g_TailNode.pPrev = pNewNode;
+}
+
+void InitList() {
+	g_HeadNode.pNext = &g_TailNode;
+	g_TailNode.pPrev = &g_HeadNode;
 }
 
 // 리스트 전체 삭제
 void ReleaseList(void) {
 	USERDATA* pTmp = g_HeadNode.pNext; // dummy head가 아니라 그 다음부터가 진짜 head다.
 	USERDATA* pDelete;
-	while (pTmp != NULL) {
+	while (pTmp != NULL && pTmp != &g_TailNode) {
 		//pNext값을 백업해놓고 free시킨다.
 		pDelete = pTmp;
 		pTmp = pTmp->pNext;
@@ -49,7 +52,7 @@ void ReleaseList(void) {
 		free(pDelete);
 	}
 
-	g_HeadNode.pNext = NULL;
+	InitList(); // 삭제 후 더미들 연결
 }
 
 USERDATA* SearchByName(const char* pszName) {
@@ -82,23 +85,15 @@ void PrintList(void) {
 	putchar('\n');
 }
 
-// 지울 노드 찾기, 삭제할 노드의 previous 노드를 반환해도 된다.(확정적으로 NULL이 아니기 때문)
-USERDATA* SearchToRemove(const char *pszName) {
-	USERDATA* pPrev = &g_HeadNode; // HEAD 노드의 이전 노드
-	while (pPrev -> pNext != NULL) {
-		if (strcmp(pPrev->pNext->name, pszName) == 0) {
-			return pPrev; // 찾은 노드의 이전 노드 주소 반환
-		}
-		pPrev = pPrev->pNext;
-	}
-
-	return NULL;
-}
-
 // 노드 지우기
-void RemoveNode(USERDATA* pPrev) { // 지울 노드의 앞 노드만 알아도 pNext로 지울 노드를 알 수 있다.
-	USERDATA* pRemove = pPrev->pNext;
-	pPrev->pNext = pRemove->pNext;
+void RemoveNode(USERDATA* pRemove) { // 지울 노드의 앞 노드만 알아도 pNext로 지울 노드를 알 수 있다.
+	/*
+		USERDATA* pPrev = pRemove -> pPrev;
+		USERDATA* pNext = pRemove -> pNext;
+		보기 힘들면 그냥 변수 선언하는게 나을수도
+	*/
+	pRemove -> pPrev -> pNext = pRemove->pNext;
+	pRemove->pNext->pPrev = pRemove->pPrev;
 	printf("RemoveNode(): %s\n", pRemove->name);
 	free(pRemove);
 }
@@ -109,7 +104,7 @@ void RemoveTest01() {
 	printf("\n------------------노드 하나 삭제 검증---------------------\n");
 	PrintList();
 	USERDATA* pPrev = NULL; // 이전 노드 저장
-	if(pPrev = SearchToRemove("Hoon")) RemoveNode(pPrev); // 지울 노드와 지울 노드의 앞 노드 검사
+	if(pPrev = SearchByName("Hoon")) RemoveNode(pPrev); // 지울 노드와 지울 노드의 앞 노드 검사
 	PrintList();
 	ReleaseList();
 }
@@ -120,9 +115,8 @@ void RemoveTest02() {
 	printf("\n---------------------3개의 노드 중 첫 번째 노드 삭제 검증---------------------\n");
 	InitDummyData();
 	PrintList();
-	if (pPrev = SearchToRemove("Hoon")) RemoveNode(pPrev);
+	if (pPrev = SearchByName("Hoon")) RemoveNode(pPrev);
 	PrintList();
-	AddNewNode(10, "Hoon", "010-1111-1111");
 	ReleaseList();
 }
 
@@ -133,9 +127,8 @@ void RemoveTest03() {
 	printf("\n---------------------3개의 노드 중 두 번째 노드 삭제 검증---------------------\n");
 	InitDummyData();
 	PrintList();
-	if (pPrev = SearchToRemove("Goon")) RemoveNode(pPrev);
+	if (pPrev = SearchByName("Goon")) RemoveNode(pPrev);
 	PrintList();
-	AddNewNode(11, "Goon", "010-1234-5678");
 	ReleaseList();
 }
 
@@ -146,13 +139,13 @@ void RemoveTest04() {
 	printf("\n---------------------3개의 노드 중 세 번째 노드 삭제 검증---------------------\n");
 	InitDummyData();
 	PrintList();
-	if (pPrev = SearchToRemove("Poon")) RemoveNode(pPrev);
+	if (pPrev = SearchByName("Poon")) RemoveNode(pPrev);
 	PrintList();
-	AddNewNode(12, "Poon", "010-9999-9999");
 	ReleaseList();
 }
 
 int main() {
+	InitList();
 	InitDummyData();
 
 	PrintList();
